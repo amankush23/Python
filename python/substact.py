8383ca9a6948a5e73739f33b7be417234b40724d
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-08-28 22:17:18 +0530

    Add files via upload

---
+++ python/substact.py
@@ -0,0 +1,10 @@
+#input section
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+
+#logic section
+result = max(a,b) - min(a,b)
+
+        
+        #display section
+print("The result of subtracting the smaller number from the greater number is",result)
