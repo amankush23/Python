5d7e3ae12245699578fa0693c0415e0765a57f77
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-01 11:45:15 +0530

    Add files via upload

---
+++ j.py
@@ -0,0 +1,11 @@
+#input section
+a=int(input("enter the height of cylinder"))
+b=int(input("enter the radius of cylinder"))
+
+
+#logic section
+V=3.14*b*b*a
+
+#display section
+
+print("the volume of cylinder",V )
