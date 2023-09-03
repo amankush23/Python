82f9666274a288be16bc9012c9cf1e7f1c98ddad
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-03 23:38:55 +0530

    area_of_cone.py

---
+++ python/area_of_cone.py
@@ -0,0 +1,9 @@
+#input section
+r=int(input("the radius of cone:"))
+h=int(input("the height of cone:"))
+
+#logic section
+area=(3.14*r**2*h)/3
+
+#display section
+print("the area of cone is",area)
