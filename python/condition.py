5a0a4abb936fc8df91f2a5674461dedddc75f83d
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-09 23:01:53 +0530

    condition.py

---
+++ python/condition.py
@@ -0,0 +1,13 @@
+#input section
+
+M= int(input("Score gained by students:"))
+
+#logic section
+if (M>=90):
+    print("A")
+elif (M>=70):
+    print("B")    
+elif (M>=50):
+    print("C")    
+elif (M>=30):
+    print("fail")    
