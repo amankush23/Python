d9d1324e9365790b5731ab2dc329a3c7ed0caeda
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-09-22 23:27:54 +0530

    max number among 3

---
+++ self practice/max number among 3.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=int(input("enter the number:"))
+
+
+if a>b and a>c:
+    print(a,"is maximum number.")
+elif b>a and b>c:
+    print(b,"is maximum number.")
+else:
+    print(c,"is the maximum number.")
