8ebdb0f7aacbf9fdd4eb4ee1501377ee0894b1f7
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2023-11-26 17:53:51 +0530

    prime_or_not.py

---
+++ Functions in Python/prime_or_not.py
@@ -0,0 +1,32 @@
+def prime(n):
+    for i in range(2,n):
+        if n % i == 0:
+            return "Not a prime number."
+            break
+    else:
+        return "prime number."
+    
+num=int(input("enter the number: "))
+result=prime(num)
+print(result)<?php
+$a=$_GET['name'];
+$b=$_GET['email'];
+$c=$_GET['password1'];
+echo $a,"<br>";
+echo $b,"<br>";
+echo $c,"<br>";
+?><?php
+$a=$_GET['name'];
+$b=$_GET['email'];
+$c=$_GET['password1'];<?php
+$a=$_GET['name'];
+$b=$_GET['email'];
+$c=$_GET['password1'];
+echo $a,"<br>";
+echo $b,"<br>";
+echo $c,"<br>";
+?>
+echo $a,"<br>";
+echo $b,"<br>";
+echo $c,"<br>";
+?>
