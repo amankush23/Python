68b2e3c34ccf8c3940b47683de4aa8ac73023fad
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2024-02-20 01:27:31 +0530

    layout.

---
+++ GUI/layout.py
@@ -0,0 +1,12 @@
+#layout
+'''layout management is the process of arranging widgets on a tkinter window in
+ python, There are three layout managers in tkinter : pack, place and grid.
+The pack()method is used to arrange widgets in a horizontal or vertical stack'''
+
+#pack 
+
+"""
+The pack() method is used to arrange widgets in a horizontal or vertical stack
+The grid() method arranges widgets in a grid similar to rows and columns
+The place() method allows you to specify the exact x and y cordinates for widgets placement
+"""
